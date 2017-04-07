c828a444c08d2d543bd8796dfe6ada3bec643bb8
jef4490 <jeffrey.hechler@gmail.com>
2017-04-07 14:33:07 -0400

    init

--- src/Init.js
+++ src/Init.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Read extends Component {
+class Init extends Component {
   constructor() {
     super()
 
   }
 
-  renderModeCompondet() {
-    if (this.props.mode === 'init') {
-      return < Init />
-    } else if (this.props.mode === 'write') {
-      return < Write />
-    } else {
-      return < Read />
-    }
-  }
-
   render() {
     return (
-      <div className="row header">
-
+      <div>
+        Init Mode
       </div>
     );
   }
