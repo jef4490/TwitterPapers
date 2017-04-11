efdae3e10ce8e3011d53f5434762530fe131210d
MartinCrane <martin.crane@gmail.com>
2017-04-11 14:50:16 -0400

    paragraph start

--- src/Read/Output.js
+++ src/Read/Output.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Paragraph from './Paragraph'
+import Sentence from './Sentence'
 import '../App.css';
 
 
 function Output(props) {
+
+    // let formattedEssay = props.tweets.map((tweet, index) => <Sentence index={index} text={tweet.text}> )
+
     return(
       <div className="Output">
-        <Paragraph tweets={props.tweets} />
+        <div className='read-paragraph'>
+          {props.tweets.length > 0 ? <h3 className="essaySubtitle">Essay:</h3> : null}
+          {props.tweets.length > 0 ? props.tweets.map((tweet, index) => <Sentence index={index} text={tweet.text}/>) : null}
+        </div>
       </div>
     )
 }
