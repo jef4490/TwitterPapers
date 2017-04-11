4327512a05cfc86a4a2f69e603b004c6a605bd07
jef4490 <jeffrey.hechler@gmail.com>
2017-04-11 13:31:39 -0400

    changed header name

--- src/Header.js
+++ src/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
   render() {
     return (
       <div className="row header" onClick={this.handleClick}>
-        <h1 className="header-text">Tweet Papers</h1>
+        <h1 className="header-text">Twitter Papers</h1>
         <h4 className="header-text">When you cant fit the profound in 140 characters.</h4>
       </div>
     );
